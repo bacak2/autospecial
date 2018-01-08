06d054b28cb932af8de2f4068168e9bdf9592c50
Krzysztof Baca <bacak2@o2.pl>
2018-01-08 14:40:51 +0100

    kontrolera i modelu importu

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/import', [
+    'uses' => 'ImportController@index',
+    'as' => 'import.index'    
+]);
