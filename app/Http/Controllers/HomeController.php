c7fd6083415db29811fbe7cdee3578e2c2947d8e
Krzysztof Baca <bacak2@o2.pl>
2018-01-15 13:36:36 +0100

    zmiana filtrowania

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\BazaDostepnychModeli;
+use App\BazaModeli;
 use App\BazaKolorowLakieru;
 
 class HomeController extends Controller
@@ -14,18 +15,21 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $filters)
     {
-        //pobrać z requestu to co przyszło getem
+
         $rows = BazaDostepnychModeli::leftJoin('baza_modelis', 'baza_dostepnych_modelis.model', '=', 'baza_modelis.model_code')
-        //dodać ->whery i ->order by
-                        ->leftJoin('baza_kolorow_lakierus', 'baza_dostepnych_modelis.kolor', '=', 'baza_kolorow_lakierus.kolor_lakieru_code')
+                ->leftJoin('baza_kolorow_lakierus', 'baza_dostepnych_modelis.kolor', '=', 'baza_kolorow_lakierus.kolor_lakieru_code')
                 ->leftJoin('baza_kolorow_tapicerkis', 'baza_dostepnych_modelis.tapicerka', '=', 'baza_kolorow_tapicerkis.kolor_tapicerki_code')
-                ->leftJoin('baza_opcji_wyposazenias', 'baza_dostepnych_modelis.opcje', '=', 'baza_opcji_wyposazenias.opcja_wyposazenia_code')
-                ->paginate(5);
+                ->leftJoin('baza_opcji_wyposazenias', 'baza_dostepnych_modelis.opcje', '=', 'baza_opcji_wyposazenias.opcja_wyposazenia_code');
         
-        $models = BazaKolorowLakieru::pluck('kolor_lakieru_decoded', 'kolor_lakieru_code');
+        if($filters->has('model')) $rows->where('model', '=', $filters->get('model')) ;
+        if($filters->has('color')) $rows->where('kolor', '=', $filters->get('color')) ;
         
+        $rows = $rows->paginate(5);
+
+        $models = BazaModeli::pluck('model_decoded', 'model_code');
+
         $colors = BazaKolorowLakieru::pluck('kolor_lakieru_decoded', 'kolor_lakieru_code');
         
         return view('main', compact('rows', 'models','colors'));
