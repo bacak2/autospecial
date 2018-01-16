8825fcfd329c1e2b985e6aea471511e7d831b719
Krzysztof Baca <bacak2@o2.pl>
2018-01-16 09:24:23 +0100

    file moving to file/ folder

--- resources/views/admin/insert-dostepny-model.blade.php
+++ resources/views/admin/insert-dostepny-model.blade.php
@@ -5,81 +5,52 @@
 {!! Form::open(['method' => 'GET', 'route' => ['dostepneModele.insert']]) !!}
 <div class='row'>
 
-    <div class='col-sm-2'>
+    <div class='col-sm-12'>
         <div class="form-group">
 
-            {!! Form::label('model_code', 'Kod') !!}
-            {!! Form::text('model_code', null, ['class'=>'form-control']) !!}
+            {!! Form::label('model_code', 'Model') !!}
+            {!! Form::select('model_code', $models, null ) !!}
 
         </div>
     </div>
-    <div class='col-sm-10'>
-        <div class="form-group">
-            {!! Form::label('model_decoded', 'Model') !!}
-            {!! Form::text('model_decoded', null, ['class'=>'form-control']) !!}
-        </div>  
-    </div>
-    @if ($errors->has('model_code'))
-        <div class="error">{{ $errors->first('model_code') }}</div>
-    @endif
 </div>
+
 <div class='row'>
-    <div class='col-sm-2'>
+
+    <div class='col-sm-12'>
         <div class="form-group">
 
-            {!! Form::label('kolor_lakieru_code', 'Kod') !!}
-            {!! Form::text('kolor_lakieru_code', null, ['class'=>'form-control']) !!}
+            {!! Form::label('kolor_lakieru_code', 'Kolor lakieru') !!}
+            {!! Form::select('kolor_lakieru_code', $lcolors, null ) !!}
 
         </div>
     </div>
-    <div class='col-sm-10'>
-        <div class="form-group">
-            {!! Form::label('kolor_lakieru_decoded', 'Kolor lakieru') !!}
-            {!! Form::text('kolor_lakieru_decoded', null, ['class'=>'form-control']) !!}
-        </div>  
-    </div>
-    @if ($errors->has('kolor_lakieru_code'))
-        <div class="error">{{ $errors->first('kolor_lakieru_code') }}</div>
-    @endif    
 </div>
+
 <div class='row'>
-    <div class='col-sm-2'>
+
+    <div class='col-sm-12'>
         <div class="form-group">
 
-            {!! Form::label('kolor_tapicerki_code', 'Kod') !!}
-            {!! Form::text('kolor_tapicerki_code', null, ['class'=>'form-control']) !!}
+            {!! Form::label('kolor_tapicerki_code', 'Kolor tapicerki') !!}
+            {!! Form::select('kolor_tapicerki_code', $tcolors, null ) !!}
 
         </div>
     </div>
-    <div class='col-sm-10'>
-        <div class="form-group">
-            {!! Form::label('kolor_tapicerki_decoded', 'Kolor tapicerki') !!}
-            {!! Form::text('kolor_tapicerki_decoded', null, ['class'=>'form-control']) !!}
-        </div>  
-    </div>
-    @if ($errors->has('kolor_tapicerki_code'))
-        <div class="error">{{ $errors->first('kolor_tapicerki_code') }}</div>
-    @endif
 </div>
+
 <div class='row'>
-    <div class='col-sm-2'>
+
+    <div class='col-sm-12'>
         <div class="form-group">
 
-            {!! Form::label('opcja_wyposazenia_code', 'Kod') !!}
-            {!! Form::text('opcja_wyposazenia_code', null, ['class'=>'form-control']) !!}
+            {!! Form::label('opcja_wyposazenia_code', 'Opcje wyposażenia') !!}
+            {!! Form::select('opcja_wyposazenia_code', $equipments, null ) !!}
 
         </div>
     </div>
-    <div class='col-sm-10'>
-        <div class="form-group">
-            {!! Form::label('opcja_wyposazenia_decoded', 'Opcje wyposażenia') !!}
-            {!! Form::text('opcja_wyposazenia_decoded', null, ['class'=>'form-control']) !!}
-        </div>  
-    </div>
-    @if ($errors->has('opcja_wyposazenia_code'))
-        <div class="error">{{ $errors->first('opcja_wyposazenia_code') }}</div>
-    @endif
 </div>
+
 {!! Form::submit('Zapisz', ['class'=>'btn btn-primary']) !!}
 {!! link_to(URL::previous(), 'Anuluj', ['class'=>'btn btn-default']) !!}
 
