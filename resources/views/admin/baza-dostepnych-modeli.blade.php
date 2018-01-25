5bff3175af399b8772b08c1a7fcbba57e4a89110
Krzysztof Baca <bacak2@o2.pl>
2018-01-25 14:46:15 +0100

    wyświetlanie nie odnalezionego kodu na czerwono

--- resources/views/admin/baza-dostepnych-modeli.blade.php
+++ resources/views/admin/baza-dostepnych-modeli.blade.php
@@ -28,10 +28,28 @@
     @foreach ($rows as $row)
     <tr>
         <td><span class="pull-left">{{ $row->komisja }}</span></td>
-        <td><span class="pull-left">{{ $row->model_decoded }}</span></td>
+        <td>
+            <span class="pull-left">
+                @if ($row->model_decoded === NULL) <span style="color:red"> {{ $row->model }} </span>
+                @else {{ $row->model_decoded }}
+                @endif
+            </span>
+        </td>
         <td><span class="pull-left">{{ $row->rok_modelowy }} </span></td>
-        <td><span class="pull-left">{{ $row->kolor_lakieru_decoded }} </span></td>
-        <td><span class="pull-left">{{ $row->kolor_tapicerki_decoded }} </span></td>
+        <td>
+            <span class="pull-left">
+                @if ($row->kolor_lakieru_decoded === NULL) <span style="color:red"> {{ $row->kolor }} </span>
+                @else {{ $row->kolor_lakieru_decoded }}
+                @endif
+            </span>
+        </td>    
+        <td>
+            <span class="pull-left">
+                @if ($row->kolor_tapicerki_decoded === NULL) <span style="color:red"> {{ $row->tapicerka }} </span>
+                @else {{ $row->kolor_tapicerki_decoded }}
+                @endif
+            </span>
+        </td>    
         <td><span class="pull-left">{{ $row->cena_dla_klienta }} </span></td>
         <!--td><a href="{{ route('dostepneModele.edit', $row) }}" class="btn btn-info">Edytuj</a></td>
         <td>
